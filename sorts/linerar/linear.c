d6336b5fc1277ce7b1659714dd9efeffa99403a7
darccau <darccau@protonmail.com>
2021-11-11 15:03:34 -0300

    feat: add linear search algorithm

---
+++ sorts/linerar/linear.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(void) {
+  const int array[] = {4,2,5,3, 27, 100, 6, 35, 14};
+  const int array_length = sizeof(array)/sizeof(int);
+  int i;
+  int j;
+  int tmp;
+
+  for (i = 0x0; i < array_length; i++) {
+    for (j = 0x0; j < array_length; j++) {
+      if (array[i] < array[j]) {
+        tmp = array[i];
+        array[i]  =  array[j];
+        array[j] = tmp;
+      }
+    }
+  }
+
+  for (i = 0x0; i < array_length; i++) {
+    printf("[%d]", array[i]);
+  }
+  puts("");
+
+  return 0x0;
+}
