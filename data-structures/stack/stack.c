c1087da03d81254389bc158ad85411fe7cad78e7
darccau <darccau@protonmail.com>
2021-11-20 07:19:18 -0300

    feat(stack): add static add method

--- data-structures/stack/stack.c
+++ data-structures/stack/stack.c
@@ -3,21 +3,35 @@
 
 
 struct stack {
-  int top;
+  int size;
+  int capacity;
+  int * top;
   int * elements;
+};
+
+struct stack * create_stack(int capacity) {
+  struct stack * new_stack = malloc(sizeof(struct stack));
+  new_stack->size = 0x0;
+  new_stack->capacity = capacity;
+  new_stack->elements = malloc(sizeof(int) * capacity);
+
+  return new_stack;
 }
 
-struct stack * create_stack(int size) {
+void add(struct stack * stack, int data) {
+  if (stack->size == stack->capacity) {
+    printf("> Stack overflow");
+    return;
+  }
 
-
-  struct stack * new_stack = malloc(sizeof(struct stack));
-  new_stack->top = size;
-  return stack;
+  stack->elements[0x0] = data;
 }
 
 int main(void) {
 
   struct stack * stack = create_stack(0x5);
+  add(stack, 0xa);
+  printf("[%d]\n", stack->elements[0x0]);
 
   return 0x0;
 }
