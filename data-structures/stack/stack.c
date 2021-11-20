a2090129d9a542066ff3d7475bbedd6663665e16
darccau <darccau@protonmail.com>
2021-11-20 10:50:47 -0300

    feat(stack): add pop method

--- data-structures/stack/stack.c
+++ data-structures/stack/stack.c
@@ -5,12 +5,13 @@
 struct stack {
   int size;
   int capacity;
-  int * top;
+  int top;
   int * elements;
 };
 
 struct stack * create_stack(int capacity) {
   struct stack * new_stack = malloc(sizeof(struct stack));
+  new_stack->top = - 0x1;
   new_stack->size = 0x0;
   new_stack->capacity = capacity;
   new_stack->elements = malloc(sizeof(int) * capacity);
@@ -23,15 +24,32 @@ void add(struct stack * stack, int data) {
     printf("> Stack overflow");
     return;
   }
-
-  stack->elements[0x0] = data;
+  stack->elements[stack->top++] = data;
 }
+
+int pop(struct stack * stack) {
+  if (stack->size > 0x0) {
+    printf("> Stack underflow");
+    return -0x1;
+  }
+
+  return stack->elements[--stack->top];
+}
+
 
 int main(void) {
 
   struct stack * stack = create_stack(0x5);
-  add(stack, 0xa);
-  printf("[%d]\n", stack->elements[0x0]);
+  add(stack, 0x0);
+  add(stack, 0x1);
+  add(stack, 0x2);
+  add(stack, 0x3);
+  add(stack, 0x4);
+  printf("> poped element [%d]\n", pop(stack));
+  printf("> poped element [%d]\n", pop(stack));
+  printf("> poped element [%d]\n", pop(stack));
+  printf("> poped element [%d]\n", pop(stack));
+  printf("> poped element [%d]\n", pop(stack));
 
   return 0x0;
 }
